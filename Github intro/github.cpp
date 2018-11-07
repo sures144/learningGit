7f98590ad9081e5eadaf0a69f60338c8fb965aed
Suresh Shrestha <sureshcstha@gmail.com>
2018-11-07 14:16:51 -0700

    New changes

---
+++ Github intro/github.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello, World!\n";
+	cout << "You all are awsome!\n";
+	cout << "You all are awsome!\n";
+
+	cout << "Thanks!\n";
+
+	//system("pause");
+	return 0;
+}
